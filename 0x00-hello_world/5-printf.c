378416dfbd5d3ee857d98c67a5768b061fc84b2c
Abdelhamid <abdlhamid.elya@gmail.com>
2023-05-18 15:25:39 -0700

    Task 5 printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main- entry point to the program
+ *
+ *Return: always 0 success
+ */
+int main(void)
+	{
+	printf("%s","with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+	}
