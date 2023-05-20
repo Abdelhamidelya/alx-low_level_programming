2043ea7146327646b3a2d8b329a92c9c317c4bb6
Abdelhamid <abdlhamid.elya@gmail.com>
2023-05-20 16:53:07 -0700

    Task 102 print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 /**
- *main- program to print the combo of 4 numbers
+ *main- program to print the combo of 2 by 4 numbers
  *
  *numbA: first number
  *
  *numbB: second number
- *
- *numbC: third number
- *
- *numbD: forth number
  *
  *Return: 0 success
  */
@@ -16,31 +12,23 @@ int main(void)
 {
 	int numbA;
 	int numbB;
-	int numbC;
-	int numbD;
 
-	numbA = 0;
-	numbB = 0;
-	numbC = 0;
-	numbD = 1;
-	for (numbA = 0 ; numbA <= 9 ; numbA++)
-	for (numbB = 0 ; numbB <= 8 ; numbB++)
-	for (numbC = 0 ; numbC <= 9 ; numbC++)
-	for (numbD = 1 ; numbD <= 9 ; numbD++)
-	if (numbA + numbB < numbC + numbD && numbA <= numbC)
+	for (numbA = 0 ; numbA <= 98 ; numbA++)
+	for (numbB = 1 ; numbB <= 99 ; numbB++)
 	{
-		putchar('0' + numbA);
-		putchar('0' + numbB);
-		putchar(' ');
-		putchar('0' + numbC);
-		putchar('0' + numbD % 10);
-		if (numbA == 9 && numbB == 8)
-		{
+	putchar('0' + numbA / 10);
+	putchar('0' + numbA % 10);
+	putchar(' ');
+	putchar('0' + numbB / 10);
+	putchar('0' + numbB % 10);
+		if(numbA == 98 && numbB == 99)
+	        {
 			break;
 		}
-		putchar(',');
-		putchar(' ');
+	putchar(',');
+	putchar(' ');
 	}
 	putchar('\n');
 	return (0);
+
 }
