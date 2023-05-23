4ba01e86468d4f3a01ff10b0518445a3ab0b3da6
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-05-23 13:58:08 +0000

    Task 1 print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_alphabet - function to print alphabets
+*
+*
+*alpha: char with starting value 'a'
+*
+*
+*/
+void print_alphabet(void)
+{
+	char c;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
