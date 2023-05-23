f64d4c21dbaf4004c55a946a08891930c988990a
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-05-23 14:37:48 +0000

    Task 4 is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_isalpha- function to check if the given letter is an alphabet
+*
+*
+*@c: the letter to check
+*
+*Return: 1 if c is alpha 0 if not
+*
+*/
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	return (1);
+	else
+	return (0);
+}
