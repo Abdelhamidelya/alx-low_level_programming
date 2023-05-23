99defe95cdd8f80645e109c40a2e61974685703d
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-05-23 16:10:41 +0000

    Task 10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* add - functio to add two numbers
+*
+*@a: first number
+*
+*
+*@b: second number
+*
+*result: int to store the value of addition
+*
+*
+*Return: the addition output
+*/
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
