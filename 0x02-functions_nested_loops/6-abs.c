7eceaebcb3cc3d643995038c8f50bbdc29dc0c3f
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-05-24 01:27:16 +0000

    Task 6 abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - main function
+ *
+ * @n: the negative num to make absolute
+ *
+ * Return: absolute int
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
+}
