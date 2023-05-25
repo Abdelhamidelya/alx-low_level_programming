ce58ead769fc2a6a4fdeb579651fd2c78fdca2d4
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-05-25 17:12:12 +0000

    Task 5 more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,14 +15,12 @@ int b;
 for (a = 0 ; a <= 9 ; a++)
 	{
 	for (b = 0 ; b <= 14 ; b++)
+		{
 		if (b > 9)
-		{
-		_putchar('0' + b / 10);
+			{
+			_putchar('0' + b / 10);
+			}
 		_putchar('0' + b % 10);
-		}
-		else
-		{
-		_putchar('0' + b);
 		}
 	_putchar('\n');
 	}
