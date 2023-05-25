1e968f39abed29193988c8af1445374aad44954e
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-05-25 14:42:49 +0000

    task 0 is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_isupper- function to check if the given letter is an alphabet
+*
+*
+*@c: the letter to check
+*
+*Return: 1 if c is uppercase 0 if not
+*
+*/
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	return (1);
+	else
+	return (0);
+}
