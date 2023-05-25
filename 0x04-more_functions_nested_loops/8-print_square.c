4c7262b4b1ee24063e2c485c6cd31e6ae10e561d
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-05-25 22:04:59 +0000

    Task 8 print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* print_square - function to print a square using #
+*
+*@size: size of the square
+*
+*count: size of the square
+*hash: number of hashes to print
+*
+*/
+void print_square(int size)
+{
+	int count;
+	int hash;
+
+	if (size <= 0)
+	{
+	putchar('\n');
+	}
+	for (count = 1 ; count <= size ; count++)
+	{
+	for (hash = 1 ; hash <= size ; hash++)
+	_putchar('#');
+	_putchar('\n');
+	}
+}
