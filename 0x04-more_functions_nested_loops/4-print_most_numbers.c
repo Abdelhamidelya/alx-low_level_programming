5924abe76e7a555a0f2506b1aaa0a03480bf9334
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-05-25 16:30:15 +0000

    Task 4 print most numbes

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
-*print_numbers- function to print all numbers except 2 and 4
+*print_most_numbers- function to print all numbers except 2 and 4
 *
 *numbers: string contains numbers except 2 and 4
 *
 *count: int to count to the end of line
 *
 */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	char *numbers;
 	int count;
 
-	numbers = "1356789";
+	numbers = "01356789\n";
 	count = 0;
 
 	while (numbers[count] != '\0')
 	{
-	putchar(numbers[count]);
-	count++;
+		_putchar(numbers[count]);
+		count++;
 	}
 }
