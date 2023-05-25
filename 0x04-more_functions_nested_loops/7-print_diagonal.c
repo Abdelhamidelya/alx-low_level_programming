0b1119c63f6d6befc79356255bfd7ef33fbc610d
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-05-25 22:45:54 +0000

    Task 7 print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,16 +10,19 @@
 */
 void print_diagonal(int n)
 {
-    int count;
-    int slash;
-	if( n <= 0)
+	int count;
+	int slash;
+
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-	for(count = 1 ; count <= n ; count++)
+	for (count = 1 ; count <= n ; count++)
 	{
-		for(slash = 1 ; slash <= count ; slash++)
+		for (slash = 1 ; slash <= count ; slash++)
+		{
 		_putchar(' ');
+		}
 		_putchar('\\');
 		_putchar('\n');
 	}
