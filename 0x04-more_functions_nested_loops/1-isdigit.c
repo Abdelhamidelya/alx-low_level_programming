a4e9b73aec333916164712646aaee1bdf18ca27f
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-05-25 14:52:09 +0000

    Task 1 is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_isdigit- function to check if n is a number
+*
+*
+*@c: value to check
+*
+*Return: 1 if n is a number 0 if not
+*
+*/
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	return (1);
+	else
+	return (0);
+}
