620da0b550aff166ad2eb6078e2fb03b7d05664f
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-05-25 17:26:06 +0000

    Task 6 print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*print_line - function to print 'n' lines
+*
+*@n: line lenght
+*
+*count: int to print the line
+*/
+void print_line(int n)
+{
+	int count;
+
+	for (count = 1 ; count <= n ; count++)
+	{
+		if (count <= 0)
+		{
+		_putchar('\n');
+		}
+		_putchar('_');
+	}
+	_putchar('\n');
+}
