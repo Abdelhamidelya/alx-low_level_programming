cc9d97249d58f7106ce854fab3a1295123902f81
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-05-25 15:09:50 +0000

    Task 3 print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_numbers- function to print numbers from 0 to 9
+*
+*numb: int to count and print from 0 to 9
+*
+*/
+void print_numbers(void)
+{
+	int numb;
+
+	numb = 0;
+	while (numb <= 9)
+	{
+	putchar('0' + numb);
+	numb++;
+	}
+	_putchar ('\n');
+}
