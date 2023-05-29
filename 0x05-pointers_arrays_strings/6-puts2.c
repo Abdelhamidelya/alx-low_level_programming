47ec7ba698460cd62c0cd19ecec115b629b00b3c
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-05-29 16:12:39 +0000

    Task 6 puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+*_strlen - function to count the lenght of a string
+*
+*@s: the string to count
+*
+*lenght: int to count to the end of line
+*
+*Return: the string length
+*/
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+	length++;
+	}
+	return (length);
+}
+/**
+*puts2 - a function that prints every other character of a string
+*
+*@str: string to print from
+*
+*count: int to print every other char
+*
+*/
+void puts2(char *str)
+{
+	int count;
+
+for (count = 0 ; count < _strlen(str) ; count++)
+	{
+	if (count % 2 == 0)
+		{
+		putchar(str[count]);
+		}
+	}
+}
