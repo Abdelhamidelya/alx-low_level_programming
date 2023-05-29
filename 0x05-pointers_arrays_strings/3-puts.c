fe4d344489958d360594062814b79fc65b46c485
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-05-29 14:42:43 +0000

    Task 3 puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*_puts - functionto print a string
+*
+*@str: the string to print
+*
+*length: int to count the length of the string
+*
+*
+*/
+void _puts(char *str)
+{
+	int length;
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+		_putchar(str[length]);
+		length++;
+	}
+	_putchar('\n');
+}
