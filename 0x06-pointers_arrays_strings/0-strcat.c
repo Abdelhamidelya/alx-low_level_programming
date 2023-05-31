d35d03a5e45af1ae00bfb312b1993221a7ceae93
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-05-31 23:26:43 +0000

    Task 0 strnat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+*_strlen - function to count the lenght of a string
+*
+*@s: the string to count
+*
+*lenght: int to count to the end of line
+*
+*Return: the string length
+*/
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+	length++;
+	}
+	return (length);
+}
+
+/**
 *_strcat - function to concatenates two strings
 *
 *@dest: destination to print from
@@ -12,5 +34,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-strcat(dest, src);
+	char *count;
+
+	count = dest + _strlen(dest);
+	while (*src != '\0')
+	{
+	*count++ = *src++;
+	}
+	return (dest);
 }
