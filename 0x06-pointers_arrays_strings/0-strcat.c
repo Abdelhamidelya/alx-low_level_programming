2c2c03e368827447a604a357721bda61394aeff5
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-05-31 23:02:43 +0000

    Task 0 strnat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_strcat - function to concatenates two strings
+*
+*@dest: destination to print from
+*
+*@src: newly printed string
+*
+*Return: nothing
+*/
+
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+}
