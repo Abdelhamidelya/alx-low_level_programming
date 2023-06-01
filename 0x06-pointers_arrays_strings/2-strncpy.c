c63dd4fc7523ec596393f2f0af36ec005297aa82
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-06-01 16:12:54 +0000

    Task 2 strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+*_strlen - function to count the lenght of a string
+*
+*@s: the string to count
+*
+*lenght: int to count to the end of line
+*
+*Return: the string length
+*/
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+	length++;
+	}
+	return (length);
+}
+
+/**
+*_strncpy - function that copies a string
+*
+*@dest: destination to print from
+*
+*@src: newly printed string
+*
+*@n: number of characters to be concatenated
+*
+*Return: copied string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *count;
+
+	count = dest;
+	while (*src != '\0' && n--)
+	{
+	*dest = *src;
+	dest++;
+	src++;
+	}
+	return (count);
+}
