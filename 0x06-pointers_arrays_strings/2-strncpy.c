65c4555948a25d48d80e5c371a9a61b0e1825ace
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-06-01 16:15:33 +0000

    Task 2 strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,28 +1,6 @@
 #include "main.h"
 
 /**
-*_strlen - function to count the lenght of a string
-*
-*@s: the string to count
-*
-*lenght: int to count to the end of line
-*
-*Return: the string length
-*/
-
-int _strlen(char *s)
-{
-	int length;
-
-	length = 0;
-	while (s[length] != '\0')
-	{
-	length++;
-	}
-	return (length);
-}
-
-/**
 *_strncpy - function that copies a string
 *
 *@dest: destination to print from
