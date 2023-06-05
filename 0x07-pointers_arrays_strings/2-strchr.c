000b0b90cffed5049463e6818bd6cca58a0dcb4a
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-06-05 23:15:38 +0000

    Task 2 strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_strchr - function to search for a char in a string
+*
+*@s: string to search in
+*
+*@c: char to search for
+*
+*Return: a string starting from "c"
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+	if (*s == c)
+	return (s);
+	s++;
+	}
+}
