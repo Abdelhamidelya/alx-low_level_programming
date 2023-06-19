8346d8d26ff0e5ec8a22956bf91ac8c6a90675bb
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-06-19 17:15:16 +0000

    Task 3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef _NUM_
+#define _NUM_ _ABS_
+#define ABS(x) ((x < 0) ? (-x) : (x))
+#endif
--- 0x0D-preprocessor/main.c
+++ 0x0D-preprocessor/main.c
@@ -1,12 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
 
 /**
-*main - program that prints the name of the file it was compiled from
-*
-*Retrun: 0 success
-*/
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	printf("File :%s\n", __FILE__ );
-	return 0;
+    int i;
+    int j;
+
+    i = ABS(-98) * 10;
+    j = ABS(98) * 10;
+    printf("%d, %d\n", i, j);
+    return (0);
 }
