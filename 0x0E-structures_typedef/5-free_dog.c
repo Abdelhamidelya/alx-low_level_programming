601abe7edccf7a2a89e40812af0848c1b322ad29
Abdelhamidelya <abdlhamid.elya@gmail.com>
2023-06-20 01:07:22 +0000

    Task 5 free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*free_dog - function that frees dogs
+*
+*@d: dog to be freed
+*
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+	free(d->owner);
+	free(d->name);
+	free(d);
+	}
+}
